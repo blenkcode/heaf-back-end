bc7a30b814277fe90ec8faf4e29fd711e7a3f734
Your name <Your email>
2024-06-12 17:56:28 +0200

    vercel

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
 var express = require("express");
-
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-const cors = require("cors");
-app.use(cors());
+
+// Configurez CORS pour autoriser uniquement votre domaine frontend
+const corsOptions = {
+  origin: "https://heaf-front-end.vercel.app", // Remplacez par le domaine de votre frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
