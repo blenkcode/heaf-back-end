3952a39a6fd9f1e3c0d498552f69375f1f7ed4a9
Your name <valentinmor.pro@gmail.com>
2024-10-16 16:26:23 +0200

    cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 var express = require("express");
-
+const cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -9,18 +9,18 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-const cors = require("cors");
+
 const corsOptions = {
-  origin: ["https://heafv2.vercel.app", "http://localhost:3000"], // Autoriser ces domaines
+  origin: ["https://heafv2.vercel.app", "http://localhost:3000"], // Origines autorisées
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Méthodes HTTP autorisées
-  credentials: true, // Autorise l'envoi de cookies ou d'headers d'authentification
-  optionsSuccessStatus: 200, // Pour les navigateurs anciens
+  credentials: true, // Autoriser les cookies si nécessaire
+  optionsSuccessStatus: 200, // Pour les navigateurs plus anciens (IE11, etc.)
 };
+app.use(cors(corsOptions));
 const port = process.env.PORT || 3005;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-app.use(cors(corsOptions));
 
 app.use(logger("dev"));
 app.use(express.json());
